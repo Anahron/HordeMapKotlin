68b6d84c9aeaa16ce3b7141abdf2f2535c813d0a
Anahron <anahron125@gmail.com>
2023-11-21 22:33:44 +0300

    corrupt kmz load fix

--- app/src/main/java/ru/newlevel/hordemap/data/storage/implementation/FilesLocalStorage.kt
+++ app/src/main/java/ru/newlevel/hordemap/data/storage/implementation/FilesLocalStorage.kt
@@ -7,19 +7,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import ru.newlevel.hordemap.app.BASE_LAST_MAP_FILENAME
 import ru.newlevel.hordemap.app.GPX_EXTENSION
-import ru.newlevel.hordemap.app.KML_EXTENSION
 import ru.newlevel.hordemap.app.KMZ_EXTENSION
-import ru.newlevel.hordemap.app.getInputSteamFromUri
 import ru.newlevel.hordemap.data.storage.interfaces.GameMapLocalStorage
 import java.io.File
-import java.io.FileOutputStream
 import java.nio.file.Files
-import java.util.zip.ZipEntry
-import java.util.zip.ZipInputStream
-import java.util.zip.ZipOutputStream
 import kotlin.io.path.Path
 
-class FilesLocalStorage(private val context: Context) : GameMapLocalStorage {
+class FilesLocalStorage(private val context: Context, private val filesUtils: FilesUtils) : GameMapLocalStorage {
     /*
      Из коробки кмл не умеет отображать файлы у которых в kml есть тег <Document>
      вынимаем kml, удаляем тег и перезаписываем в архив новый файл
@@ -32,9 +26,9 @@ class FilesLocalStorage(private val context: Context) : GameMapLocalStorage {
             Files.deleteIfExists(Path(gpxFile.toString()))
             if (suffix == KMZ_EXTENSION) try {
                 kmzFile.createNewFile()
-                val kmlContent = extractKMLContentFromKMZ(uri)
-                val modifiedKMLContent = removeDocumentTags(kmlContent)
-                replaceKMLInKMZ(uri, kmzFile, modifiedKMLContent)
+                val kmlContent = filesUtils.extractKMLContentFromKMZ(uri, context)
+                val modifiedKMLContent = filesUtils.removeDocumentTags(kmlContent)
+                filesUtils.replaceKMLInKMZ(uri, kmzFile, modifiedKMLContent, context)
             } catch (e: Exception) {
                 e.printStackTrace()
             }
@@ -49,59 +43,6 @@ class FilesLocalStorage(private val context: Context) : GameMapLocalStorage {
             }
         }
         return File(context.filesDir, BASE_LAST_MAP_FILENAME + suffix).toUri()
-    }
-
-    private fun replaceKMLInKMZ(oldKmzFile: Uri, newKmzFile: File, newKMLContent: String) {
-        context.contentResolver.openInputStream(oldKmzFile).use {
-            ZipInputStream(it).use { zipInputStream ->
-                ZipOutputStream(FileOutputStream(newKmzFile)).use { zipOutputStream ->
-                    var entry: ZipEntry?
-                    while (zipInputStream.nextEntry.also { entry = it } != null) {
-                        if (entry?.name?.endsWith(KML_EXTENSION) == true) {
-                            zipOutputStream.putNextEntry(ZipEntry(entry?.name))
-                            zipOutputStream.write(newKMLContent.toByteArray())
-                            zipOutputStream.closeEntry()
-                        } else {
-                            zipOutputStream.putNextEntry(ZipEntry(entry?.name))
-                            val buffer = ByteArray(1024)
-                            var bytesRead: Int
-                            while (zipInputStream.read(buffer).also { bytesRead = it } != -1) {
-                                zipOutputStream.write(buffer, 0, bytesRead)
-                            }
-                            zipOutputStream.closeEntry()
-                        }
-                        zipInputStream.closeEntry()
-                    }
-                }
-            }
-        }
-    }
-
-    private suspend fun extractKMLContentFromKMZ(uri: Uri): String {
-        val buffer = ByteArray(10024)
-        val stringBuilder = StringBuilder()
-        getInputSteamFromUri(uri, context).use {
-            ZipInputStream(it).use { zipInputStream ->
-                var entry: ZipEntry?
-                while (zipInputStream.nextEntry.also { entry = it } != null) {
-                    if (entry?.name?.endsWith(KML_EXTENSION) == true) {
-                        var bytesRead: Int
-                        while (zipInputStream.read(buffer).also { bytesRead = it } != -1) {
-                            stringBuilder.append(buffer.decodeToString(0, bytesRead))
-                        }
-                        zipInputStream.closeEntry()
-                        return stringBuilder.toString()
-                    }
-                }
-            }
-        }
-        return ""
-    }
-
-    private fun removeDocumentTags(kmlContent: String): String {
-        return kmlContent
-            .replace("<Document>", "")
-            .replace("</Document>", "")
     }
 
     override suspend fun loadLastMapFromFile(): Uri? {
---
+++ app/src/main/java/ru/newlevel/hordemap/data/storage/implementation/FilesUtils.kt
@@ -0,0 +1,67 @@
+package ru.newlevel.hordemap.data.storage.implementation
+
+import android.content.Context
+import android.net.Uri
+import ru.newlevel.hordemap.app.KML_EXTENSION
+import ru.newlevel.hordemap.app.getInputSteamFromUri
+import java.io.File
+import java.io.FileOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
+class FilesUtils {
+    fun replaceKMLInKMZ(oldKmzFile: Uri, newKmzFile: File, newKMLContent: String, context: Context) {
+        context.contentResolver.openInputStream(oldKmzFile).use {
+            ZipInputStream(it).use { zipInputStream ->
+                ZipOutputStream(FileOutputStream(newKmzFile)).use { zipOutputStream ->
+                    var entry: ZipEntry?
+                    while (zipInputStream.nextEntry.also { entry = it } != null) {
+                        if (entry?.name?.endsWith(KML_EXTENSION) == true) {
+                            zipOutputStream.putNextEntry(ZipEntry(entry?.name))
+                            zipOutputStream.write(newKMLContent.toByteArray())
+                            zipOutputStream.closeEntry()
+                        } else {
+                            zipOutputStream.putNextEntry(ZipEntry(entry?.name))
+                            val buffer = ByteArray(1024)
+                            var bytesRead: Int
+                            while (zipInputStream.read(buffer).also { bytesRead = it } != -1) {
+                                zipOutputStream.write(buffer, 0, bytesRead)
+                            }
+                            zipOutputStream.closeEntry()
+                        }
+                        zipInputStream.closeEntry()
+                    }
+                }
+            }
+        }
+    }
+
+   suspend fun extractKMLContentFromKMZ(uri: Uri, context: Context): String {
+        val buffer = ByteArray(10024)
+        val stringBuilder = StringBuilder()
+        getInputSteamFromUri(uri, context).use {
+            ZipInputStream(it).use { zipInputStream ->
+                var entry: ZipEntry?
+                while (zipInputStream.nextEntry.also { entry = it } != null) {
+                    if (entry?.name?.endsWith(KML_EXTENSION) == true) {
+                        var bytesRead: Int
+                        while (zipInputStream.read(buffer).also { bytesRead = it } != -1) {
+                            stringBuilder.append(buffer.decodeToString(0, bytesRead))
+                        }
+                        zipInputStream.closeEntry()
+                        return stringBuilder.toString()
+                    }
+                }
+            }
+        }
+        return ""
+    }
+
+   fun removeDocumentTags(kmlContent: String): String {
+        return kmlContent
+            .replace("<Document>", "")
+            .replace("</Document>", "")
+    }
+
+}
